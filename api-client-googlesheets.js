f6b1c884a9584be76976afa080c52b58b4814da9
FOR ALL HOME CARE <admin@forallhomecare.com>
2026-02-06 13:18:34 -0700

    Configure Google Sheets API URL

--- api-client-googlesheets.js
+++ api-client-googlesheets.js
@@ -1,7 +1,7 @@
 // API Client for Home Care System - Google Sheets Backend
 // Replace YOUR_GOOGLE_APPS_SCRIPT_URL_HERE with your actual deployment URL
 
-const GOOGLE_SHEETS_API_URL = 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE';
+const GOOGLE_SHEETS_API_URL = 'https://script.google.com/macros/s/AKfycbwpCEVZ7cJBBEHV8SYlWmyv9XROGNYlwL-TGWu9ctlUCRTDOTVnCZ2boqzMwlvCKlzvfVw/exec';
 
 class HomeCareAPI {
   // Submit appointment with evaluation
